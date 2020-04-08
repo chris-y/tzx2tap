9ffc57e6f3877c2e2082578ac9d072c034f29782
Chris Young <chris@unsatisfactorysoftware.co.uk>
2020-04-08 12:10:39 +0100

    Remove most of our error messages and just use the esxDOS/BASIC error codes

--- tzx2tap.c
+++ tzx2tap.c
@@ -31,9 +31,8 @@ static uint32_t Get3(char *mem) { return(mem[0]+(mem[1]*256UL)+(mem[2]*256UL*256
 static uint32_t Get4(char *mem) { return(mem[0]+(mem[1]*256UL)+(mem[2]*256UL*256UL)+(mem[3]*256UL*256UL*256UL)); }
 
 // exits with an error message *errstr
-static void Error(char *errstr, int eno)
+static void Error(int eno)
 {
-  printf("\nError: %s\n",errstr);
   ZXN_NEXTREGA(REG_TURBO_MODE, old_cpu_speed);
   exit(eno);
 }
@@ -60,7 +59,7 @@ static uint32_t FileLength(unsigned char fh)
 
   errno = 0;
   if(esx_f_fstat(fh, (struct esx_stat *)&es)) {
-    Error("unable to stat file", errno);
+    Error(errno);
   }
   return(es.size);
 }
@@ -74,7 +73,7 @@ static uint32_t read_file(unsigned char fh, char *mem, uint32_t posn)
 }
 
 // read chunks of 1K from input and write to output
-static void convert_data(unsigned char fhi, unsigned char fho, uint32_t posn, uint32_t len)
+static int convert_data(unsigned char fhi, unsigned char fho, uint32_t posn, uint32_t len)
 {
   char *buf;
   uint32_t bytes_read = 0;
@@ -83,7 +82,7 @@ static void convert_data(unsigned char fhi, unsigned char fho, uint32_t posn, ui
   buf=(char *) malloc(1024);
 
   if(buf==NULL) 
-    Error("Not enough memory to convert", ERRB_4_OUT_OF_MEMORY);
+    return ERRB_4_OUT_OF_MEMORY;
 
   esx_f_seek(fhi, posn, ESX_SEEK_SET);
 
@@ -101,6 +100,7 @@ static void convert_data(unsigned char fhi, unsigned char fho, uint32_t posn, ui
   }
 
   free(buf);
+  return 0;
 }
 
 int main(int argc, char *argv[])
@@ -117,6 +117,7 @@ int main(int argc, char *argv[])
   uint32_t start;
   long loop_start = 0;
   int loop_count = 0;
+  int err = 0;
 
   printf("TZX2TAP for NextZXOS v%s\nby Chris Young 2020\ngithub.com/chris-y/tzx2tap\n", PROGVER);
   printf("Based on ZXTape Utilities\nTZX to TAP Converter v0.13b\n");
@@ -143,16 +144,17 @@ int main(int argc, char *argv[])
   fhi = esx_f_open(argv[1], ESX_MODE_READ);
 
   if(errno) 
-    Error("Input file not found", errno);
+    Error(errno);
 
   errno = 0;
   fho = esx_f_open(buf, ESX_MODE_WRITE | ESX_MODE_OPEN_CREAT_NOEXIST);
 
   if(errno) {
     if(errno == ESX_EEXIST) {
-      Error("Output file exists\nProbably already converted!", 0);
+      printf("Error: Output file exists\nProbably already converted!\n");
+      Error(0);
     } else {
-      Error("Cannot create output file", errno);
+      Error(errno);
     }
   }
 
@@ -160,19 +162,22 @@ int main(int argc, char *argv[])
   mem=(char *) malloc(MAX_HEADER_SIZE);
 
   if(mem==NULL) 
-   Error("Not enough memory", ERRB_4_OUT_OF_MEMORY);
+   Error(ERRB_4_OUT_OF_MEMORY);
 
   esx_f_read(fhi,mem,10); mem[7]=0;
 
   if(strcmp(mem,"ZXTape!")) { 
     free(mem);
-    Error("Input is not TZX format", ESX_EWRTYPE); 
+    Error(ESX_EWRTYPE); 
   }
 
   printf("\nZXTape file revision %d.%02d\n",mem[8],mem[9]);
 
-  if(!mem[8]) 
-    Error("\nTZX dev ver not supported", ESX_EWRTYPE);
+  if(!mem[8]) {
+    free(mem);
+    printf("Error: TZX dev ver not supported\n");
+    Error(ESX_EWRTYPE);
+  }
 
   if(mem[8]>MAJREV) 
     printf("\nWarning: Some blocks may not be recognised and used\n");
@@ -189,8 +194,7 @@ int main(int argc, char *argv[])
 
   printf("\nConverting...");
 
-  while(pos<flen)
-    {
+  while(pos<flen) {
     pos++;
     p = pos - start;
 
@@ -199,20 +203,34 @@ int main(int argc, char *argv[])
     switch(mem[p-1])
       {
       case 0x10: len=Get2(&mem[p+0x02]);
-                 convert_data(fhi, fho, pos+0x02, 2);
-                 convert_data(fhi, fho, pos+0x04, len);
-
+                 err = convert_data(fhi, fho, pos+0x02, 2);
+                 if(err) {
+                   free(mem);
+                   Error(err);
+                 }
+                 err = convert_data(fhi, fho, pos+0x04, len);
+                 if(err) {
+                   free(mem);
+                   Error(err);
+                 }
                  pos+=len+0x04;
                  start = read_file(fhi, mem, pos);
                  block++;
                  break;
       case 0x11: len=Get3(&mem[p+0x0F]);
-                 if(len<65536)
-                   {
-                   convert_data(fhi, fho, pos+0x0F, 2);
-                   convert_data(fhi, fho, pos+0x12, len);
-                   block++;
+                 if(len<65536) {
+                   err = convert_data(fhi, fho, pos+0x0F, 2);
+                   if(err) {
+                     free(mem);
+                     Error(err);
                    }
+                   err = convert_data(fhi, fho, pos+0x12, len);
+                   if(err) {
+                     free(mem);
+                     Error(err);
+                   }
+                   block++;
+                 }
                  else 
                    longer=true;
                  custom=true;
@@ -228,12 +246,19 @@ int main(int argc, char *argv[])
                  start = read_file(fhi, mem, pos);
                  break;
       case 0x14: len=Get3(&mem[p+0x07]);
-                 if(len<65536)
-                   {
-                   convert_data(fhi, fho, pos+0x07, 2);
-                   convert_data(fhi, fho, pos+0x0A, len);
-                   block++;
+                 if(len<65536) {
+                   err = convert_data(fhi, fho, pos+0x07, 2);
+                   if(err) {
+                     free(mem);
+                     Error(err);
                    }
+                   err = convert_data(fhi, fho, pos+0x0A, len);
+                   if(err) {
+                     free(mem);
+                     Error(err);
+                   }
+                   block++;
+                 }
                  else 
                    longer=true;
                  dataonly=true;
